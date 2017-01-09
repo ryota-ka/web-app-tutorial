b320328339ee60210b9347d0d2873610660c41d0
Ryota Kameoka <kameoka.ryota@gmail.com>
2017-01-09 20:38:24 +0900

    Define root and hello endpoints

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Data.IORef
+import Data.Monoid
+import Web.Spock
+import Web.Spock.Config
 
 main :: IO ()
-main = someFunc
+main = do
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock 8080 $ (spock spockCfg) app
+
+app :: SpockM () () () ()
+app = do
+  get root $ do
+    text "Hello, World!"
+
+  get ("hello" <//> var) $ \name -> do
+    text $ "Hello, " <> name <> "!"
