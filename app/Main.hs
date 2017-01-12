f275ac510d8c07f4661fcad8c2c978c5a91d47d9
Ryota Kameoka <kameoka.ryota@gmail.com>
2017-01-12 23:44:57 +0900

    Add basic routings

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Data.Monoid ((<>))
+import Network.HTTP.Types
+import Web.Scotty
+import qualified Data.Text.Lazy as T
 
 main :: IO ()
-main = someFunc
+main = do
+  scotty 8080 $ do
+    get "/" $ do
+      text "Hello, world!"
+
+    get "/hello/:name" $ do
+      name <- param "name"
+      text $ "Hello, " <> name <> "!"
+
+    get "/redirect/to/root" $ do
+      status status302
+      setHeader "X-Foo-Bar" "bazqux"
+      redirect "/"
