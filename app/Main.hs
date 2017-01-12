33976df25717eaefc534d8c961f0f40f33491cd8
Ryota Kameoka <kameoka.ryota@gmail.com>
2017-01-13 01:03:32 +0900

    Add endpoint GET /tasks

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,28 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 module Main where
 
+import Data.Aeson (ToJSON)
 import Data.Monoid ((<>))
+import GHC.Generics (Generic)
 import Network.HTTP.Types
 import Web.Scotty
 import qualified Data.Text.Lazy as T
+
+data Task = Task {
+    taskId :: Int
+  , title  :: T.Text
+  } deriving (Eq, Generic, Show)
+
+instance ToJSON Task
+
+defaultTasks :: [Task]
+defaultTasks = [
+    Task 1 "Haskellの勉強会を探す"
+  , Task 2 "CAMPHOR- BASEの場所を調べる"
+  , Task 3 "Haskellの文法を勉強する"
+  , Task 4 "ScottyでWebアプリケーションを作る"
+  ]
 
 main :: IO ()
 main = do
@@ -21,3 +38,6 @@ main = do
       status status302
       setHeader "X-Foo-Bar" "bazqux"
       redirect "/"
+
+    get "/tasks" $ do
+      json defaultTasks
